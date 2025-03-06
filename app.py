5fe5f90d7ee7e47be2a264be698911ad47b69c52
ataschuk <alextaschuk@gmail.com>
2025-03-06 12:28:02 -0800

    main clock logic complete

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # imports for time stuff
-from datetime import datetime, timedelta                   # for getting the times
+from datetime import datetime, timedelta, timezone                   # for getting the times
 import os
 import glob
 import random
-from crontab import CronTab
 
 # Libraries for displaying image on screen 
 import sys
@@ -24,12 +23,12 @@ class Clock:
 
 
     def __init__(self):
-        self.picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'image') # path to .bmp files
+        self.picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'BookQuoteClock/images') # path to .bmp files
         self.libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib') # path to 
         if os.path.exists(self.libdir):
             sys.path.append(self.libdir) # TODO: figure out what this does
         logging.basicConfig(level=logging.DEBUG) 
-        self.time = datetime.now()
+        self.time = datetime.now(timezone.utc)
         print('clock obj was made.')
 
     def get_minute(self) -> int:
@@ -44,7 +43,7 @@ class Clock:
         return str(hour) + str(minute)
     
     def update_time(self) -> datetime:
-        self.time = datetime.now()
+        self.time = datetime.now(timezone.utc)
                                     
     def get_quotes(self, filename) -> list: # return all possible quotes for a given minute. helper function for buffer_quotes
         quotes = []
@@ -55,6 +54,7 @@ class Clock:
 
     # Initializes and/or updates the buffer
     def buffer_quotes(self) -> list:
+        print('buffer_quote called')
         self.update_time()
         curr_minute = self.get_minute()
         curr_hour = self.get_hour()
@@ -87,16 +87,16 @@ class Clock:
             else: # we are not on the 59th minute, so we don't need the next hour
                 self.time = self.time.replace(second=0) + timedelta(minutes=2)
                 curr_minute = self.get_minute()
-            
             curr_time = self.get_time(curr_hour, curr_time) # update the time
             quotes = self.get_quotes('quote_' + curr_time + '_*' + '.bmp') # get the list of quotes for the 2-minutes-ahead of current quote
             filename = 'quote_' + curr_time + '_' + str(random.randrange(0, len(quotes))) + '.bmp' # get the .bmp file for the 2-minutes-ahead quote
             quote_buffer.append(filename) # add 2-minutes-ahead quote to buffer
-            print(quote_buffer)
-            return quote_buffer
+            print('buffer quote finish')
+        return quote_buffer
 
     def display_quote(self, buffer):
         try:
+            print('display_quote was called')
             logging.info("Book Quote Clock")
             #epd = self.epd7in5_V2.EPD()
 
@@ -109,25 +109,41 @@ class Clock:
             quote = Image.open(os.path.join(self.picdir, filename))
             #epd.display(epd.getbuffer(quote))
             time.sleep(2)
+            print('display_quote finish')
         except IOError as e:
             logging.info(e)
+    
 
     def main(self):
         """
         use the python crontab library to schedule main to be called once a minute
         in main, we call buffer_quotes and display_quote
         """ 
+        print('main was called.')
         quote_buffer = self.buffer_quotes()               # buffer the current quote, and the following two quotes to display
         self.display_quote(quote_buffer)                          # display the current quote
+        print('quote was displayed')
         
         print('main')
 
 if __name__ == '__main__':
     try:
-        cron = CronTab(user = 'username') # TODO: maybe move the cronjob and calling main to a separate file?
-        job = cron.new(command = 'python3 app.py')
-        clock = Clock()
-        clock.main()
+        clock = Clock() 
+        '''
+        We will use current_minute and next_minute to determine how long the program should sleep
+        when it is started, because it won't start exactly at the 0th second of a minute; it will 
+        start when the Rpi is plugged in and boots up.
+        '''
+        while True:
+            clock.main()
+            current_seconds = time.time() # get the number of seconds since the UTC epoch for the current time
+            next_minute = datetime.now(timezone.utc) # update the clock's time again just to be safe
+            next_minute= next_minute.replace(second=0, microsecond=0) + timedelta(minutes=1) # the next minute
+            next_minute = datetime.timestamp(next_minute) # get the number of seconds since the UTC epoch for the next minute
+            sleep_time = int(next_minute - current_seconds) # get the difference between the two and sleep (i.e., wait until the next minute to display the next quote)
+            print("sleep for: " + str(sleep_time) + " seconds")
+            time.sleep(sleep_time) # sleep until next minute
+
     except KeyboardInterrupt as e:
         logging.info(e)
         #clock.epd7in5_V2.epdconfig.module_exit(cleanup=True)
