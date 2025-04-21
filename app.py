4e7b2516f2d2643d93a94571d02032763691520e
ataschuk <alextaschuk@gmail.com>
2025-04-20 21:47:12 -0700

    doing a full refresh after every image to see if that helps.

--- app.py
+++ app.py
@@ -187,6 +187,7 @@ class Clock:
             logging.info('the current time is: ' + str(self.time))
             quote_to_display = self.quote_buffer[0]                 # get the quote for the current time
             #self.epd.init_fast()                                    # speeds up updates, according to waveshare support
+            self.epd.init()                     
             self.epd.display(self.epd.getbuffer(quote_to_display))  # display the quote
             self.epd.sleep() # put screen to sleep to increase its lifespan
             self.quote_buffer[0].close() # close the image
