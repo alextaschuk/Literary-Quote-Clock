beb02f474041cf59a2fb197f3808761c36e4c9de
alextaschuk <120518938+alextaschuk@users.noreply.github.com>
2026-04-06 12:43:03 -0700

    Comment out log messages

--- update_clock.sh
+++ update_clock.sh
@@ -3,11 +3,11 @@
 # This is ran by a Cronjob once a day at 5:00 AM 
 # to pull any changes from the remote repository
 
-echo Stopping clock.service > /home/alex/Desktop/log.txt
+#echo Stopping clock.service > /home/alex/Desktop/log.txt
 sudo systemctl stop clock.service
 
-echo Attempting to pull changes from the remote repo >> /home/alex/Desktop/log.txt
+#echo Attempting to pull changes from the remote repo >> /home/alex/Desktop/log.txt
 git pull
 
-echo Restarting the Pi >> /home/alex/Desktop/log.txt
+#echo Restarting the Pi >> /home/alex/Desktop/log.txt
 sudo shutdown -r now
